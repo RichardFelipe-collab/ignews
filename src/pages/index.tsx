2f87b46e895b071e3f7f68605b3312a32a265ad9
Richard Felipe Da Silva Oliveira <richardfelipe.123@hotmail.com>
2023-02-08 19:53:07 -0300

    add sass

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/home.module.scss'
+
+export default function Products(){
+  return (
+    <h1 className={styles.title}>
+      
+      Hello
+      
+      </h1>
+  )
+}

4b1675f6cfec3adfbaaa8b154201378deed98e2a
RichardFelipe-collab <richardfelipe.123@hotmail.com>
2023-03-02 12:53:29 -0300

    authenticação ok

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,16 @@ import { Header } from '../components/Header'
 
 import '../styles/global.scss'
 
+import { SessionProvider as NextAuthProvider} from 'next-auth/react';
+
 
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
     <Header/>
     <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   )
 }
